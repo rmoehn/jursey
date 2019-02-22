b9b8e8984d87a796e66cbac968f0eab57ddc9944
Richard Möhn <richard.moehn@posteo.de>
2019-02-22 10:13:15 +0900

    SNAPSHOT Start thinking about diffing

--- src/jursey/repl_ui.clj
+++ src/jursey/repl_ui.clj
@@ -68,3 +68,51 @@
       (transcriptor/run "test/repl_ui.repl"))
 
   )
+
+(comment
+
+  (reset)
+
+  (ask-root "The root question doesn't matter.")
+  (start-working)
+  (ask "What is the mass of [1] [three-pound loaf] in [kg]?")
+  (ask "What is the mass of [20] [three-pound loaf] in [kg]?")
+  (unlock "sq.0.a")
+
+  (unlock "q.1")
+  (unlock "q.3")
+  (unlock "q.5")
+  (ask "1 pound = ? kg")
+  (unlock "sq.0.a")
+  (reply "approx. 0.5")
+  (reply "approx. 1.5 kg")
+
+  (unlock "sq.1.a")
+  ;; Multi-unlocks would be better for automation here.
+  (unlock "q.3")
+  (unlock "q.5")
+  (ask "1 pound = ? kg")
+  (unlock "sq.0.a")
+  (ask "What is 20 * 1.5 kg?")
+  (reply "$sq.1.a")
+
+  (unlock "r")
+  (unlock "r.4")
+  (ask "Give me a diff of the last versions of $r.4.children.0 and $r.4.children.1.")
+  (unlock "sq.2.a")
+
+  (ask "Give me the last version of $q.1.")
+  (ask "Give me the last version of $q.3.")
+  (ask "Are [the question of $sq.0.a] and [the question of $sq.1.a] equal?")
+  (ask "Zip [the sub-qas of $sq.0.a] and [$sq.1.a].")
+
+  )
+
+(defn diff [ws1 ws2]
+  (let [q-diff (if (= (get ws1 "q") (get ws2 "q"))
+                 [{}                  {}                  {"q" (get ws1 "q")}]
+                 [{"q" (get ws1 "q")} {"q" (get ws2 "q")} {}])]))
+
+(comment
+
+  )
